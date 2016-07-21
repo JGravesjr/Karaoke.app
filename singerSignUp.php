78c6afc98be21de791a7593211077b4f06f2e0ec
J Graves Jr <jgravesjr@outlook.com>
2016-07-20 22:30:55 -0400

    Added a submit buttom to the form.

--- singerSignUp.php
+++ singerSignUp.php
@@ -1,12 +1,18 @@
+<?php 
+	require_once(functions.php);
+		
+?>
+
 <html lang="en">
 	<head>
 		<title>Karaoke Sign Up</title>
 	</head>
 	<body>
-		<form>
+		<form action="singerSignUp.php" method="post">
 			First Name: <input type="text" name="firstname"><br>
 			Last Name: <input type="text" name="lastname"><br>
 			Song Choice: <input type="text" name="songchoice"><br>
+			<input type="submit" value="Submit">
 		</form>
 	</body>
 </html>
